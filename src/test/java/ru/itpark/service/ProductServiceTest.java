8e9731e1cb8f9f16f1f93ac79a58a8d85b23d2fa
KonAnd13 <a.mailbox13@gmail.com>
2019-10-24 12:12:46 +0300

    Added tests for sorting

--- src/test/java/ru/itpark/service/ProductServiceTest.java
+++ src/test/java/ru/itpark/service/ProductServiceTest.java
@@ -90,4 +90,82 @@ class ProductServiceTest {
         }
         assertTrue(test);
     }
+
+    @Test
+    void sortByName() {
+        target = new ArrayList<>(Arrays.asList(
+                new Tv(6, "LG a", "телевизоры", 5, 15000),
+                new Tv(8, "LG e", "телевизоры", 3, 9000),
+                new Tv(7, "LG g", "телевизоры", 5, 47000),
+                new Tv(10, "LG m", "телевизоры", 5, 17000),
+                new Tv(9, "LG v", "телевизоры", 4, 13000),
+                new Smartphone(3, "Xiaomi a", "смартфоны", 2, 6000),
+                new Smartphone(1, "Xiaomi b", "смартфоны", 1, 8000),
+                new Smartphone(2, "Xiaomi c", "смартфоны", 3, 9000),
+                new Smartphone(4, "Xiaomi d", "смартфоны", 2, 7000),
+                new Smartphone(5, "Xiaomi e", "смартфоны", 4, 2000))
+        );
+        List<Product> result = service.sortByName();
+        for (int i = 0; i < target.size(); i++) {
+            if (result.get(i).getId() == target.get(i).getId()) {
+                test = true;
+            } else {
+                test = false;
+                break;
+            }
+        }
+        assertTrue(test);
+    }
+
+    @Test
+    void sortByRating() {
+        target = new ArrayList<>(Arrays.asList(
+                new Tv(6, "LG a", "телевизоры", 5, 15000),
+                new Tv(7, "LG g", "телевизоры", 5, 47000),
+                new Tv(10, "LG m", "телевизоры", 5, 17000),
+                new Smartphone(5, "Xiaomi e", "смартфоны", 4, 2000),
+                new Tv(9, "LG v", "телевизоры", 4, 13000),
+                new Smartphone(2, "Xiaomi c", "смартфоны", 3, 9000),
+                new Tv(8, "LG e", "телевизоры", 3, 9000),
+                new Smartphone(3, "Xiaomi a", "смартфоны", 2, 6000),
+                new Smartphone(4, "Xiaomi d", "смартфоны", 2, 7000),
+                new Smartphone(1, "Xiaomi b", "смартфоны", 1, 8000))
+        );
+        List<Product> result = service.sortByRating();
+        for (int i = 0; i < target.size(); i++) {
+            if (result.get(i).getId() == target.get(i).getId()) {
+                test = true;
+            } else {
+                test = false;
+                break;
+            }
+        }
+        assertTrue(test);
+    }
+
+    @Test
+    void sortByPrice() {
+        target = new ArrayList<>(Arrays.asList(
+                new Smartphone(5, "Xiaomi e", "смартфоны", 4, 2000),
+                new Smartphone(3, "Xiaomi a", "смартфоны", 2, 6000),
+                new Smartphone(4, "Xiaomi d", "смартфоны", 2, 7000),
+                new Smartphone(1, "Xiaomi b", "смартфоны", 1, 8000),
+                new Smartphone(2, "Xiaomi c", "смартфоны", 3, 9000),
+                new Tv(8, "LG e", "телевизоры", 3, 9000),
+                new Tv(9, "LG v", "телевизоры", 4, 13000),
+                new Tv(6, "LG a", "телевизоры", 5, 15000),
+                new Tv(10, "LG m", "телевизоры", 5, 17000),
+                new Tv(7, "LG g", "телевизоры", 5, 47000))
+        );
+        List<Product> result = service.sortByPrice();
+        for (int i = 0; i < target.size(); i++) {
+            if (result.get(i).getId() == target.get(i).getId()) {
+                test = true;
+            } else {
+                test = false;
+                break;
+            }
+        }
+        assertTrue(test);
+    }
 }
