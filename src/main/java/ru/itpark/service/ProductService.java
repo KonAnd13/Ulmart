1d33d84e24a6cea516ca117c787d76d70b28b3cc
KonAnd13 <a.mailbox13@gmail.com>
2019-11-01 13:48:56 +0300

    Removed annotation @Data. Added getter for products for convenience of tests.

--- src/main/java/ru/itpark/service/ProductService.java
+++ src/main/java/ru/itpark/service/ProductService.java
@@ -1,15 +1,17 @@
 package ru.itpark.service;
 
-import lombok.Data;
 import ru.itpark.comparator.SearchByNameAscComparator;
 import ru.itpark.comparator.SearchByRatingDescComparator;
 import ru.itpark.model.Product;
 
 import java.util.*;
 
-@Data
 public class ProductService {
     private final List<Product> products = new ArrayList<>();
+
+    public List<Product> getProducts() {
+        return products;
+    }
 
     public void add(Product ... product) {
         Collections.addAll(products, product);
