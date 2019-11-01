4c1e3da922aab11bb098916027dd41b554e25271
KonAnd13 <a.mailbox13@gmail.com>
2019-11-01 14:39:24 +0300

    Made the class Product abstract

--- src/main/java/ru/itpark/model/Product.java
+++ src/main/java/ru/itpark/model/Product.java
@@ -2,11 +2,10 @@ package ru.itpark.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
-public class Product implements Comparable<Product> {
+public abstract class Product implements Comparable<Product> {
     private int id;
     private String name;
     private String category;
